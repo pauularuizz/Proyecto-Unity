8106f3ac2d572f42c1c8e9956d450bdec0f9f173
AlbertMerino <amerinoc@edu.tecnocampus.cat>
2022-05-01 22:43:12 +0200

    Movimiento + Camara +  Animaciones

--- Proyecto Creación/Assets/PlayerMovement.cs
+++ Proyecto Creación/Assets/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     float _horizontal;
     float _vertical;
     public float speed = 0.5f;
+    public Animator animator; 
 
     private Rigidbody2D _rigidbody;
 
@@ -19,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
@@ -37,6 +38,46 @@ public class PlayerMovement : MonoBehaviour
         _horizontal = inputValue.Get<Vector2>().x;
         _vertical = inputValue.Get<Vector2>().y;
 
+
+        animator.SetBool("idle", true); 
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("Idle->Arriba", true);
+            animator.SetBool("idle", false);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetBool("Idle->Abajo", true);
+            animator.SetBool("idle", false);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("idle->Izquierda", true);
+            animator.SetBool("idle", false);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetBool("idle->Derecha", true);
+            animator.SetBool("idle", false);
+        }
+        //jsadadadsasdsadascacacsacsasc
+        if (!Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("Idle->Arriba", false);
+        }
+        if (!Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetBool("Idle->Abajo", false);
+        }
+        if (!Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("idle->Izquierda", false);
+        }
+        if (!Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetBool("idle->Derecha", false);
+        }
 
 
         MoveWithVelocity();

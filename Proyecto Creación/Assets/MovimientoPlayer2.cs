77bad73b427041f71d68b33676e6f099c95fa5f4
IanRoche <97619360+IanRoche@users.noreply.github.com>
2022-05-16 17:22:57 +0200

    animaiones arregladas con cambio de sprite

---
+++ Proyecto Creación/Assets/MovimientoPlayer2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPlayer2 : MonoBehaviour
+{
+    [SerializeField]private float speed=3;
+
+    private Rigidbody2D playerRb;
+    private Vector2 moveInput;
+    private Animator playerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //inputs
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(moveX, moveY).normalized;
+
+        playerAnimator.SetFloat("Horizontal", moveX);
+        playerAnimator.SetFloat("Vertical", moveY);
+        playerAnimator.SetFloat("Speed", moveInput.sqrMagnitude);
+      
+    }
+    private void FixedUpdate()
+    {
+        //fisicas
+        playerRb.MovePosition(playerRb.position + moveInput * speed * Time.fixedDeltaTime);
+
+    }
+}

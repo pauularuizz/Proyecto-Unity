1f545e8625f26a0df66f0901ba3e896fd158242c
Paula Ruiz Pareta <pruizpa@edu.tecnocampus.cat>
2022-05-23 18:17:35 +0200

    otro push

---
+++ Proyecto Creación/Assets/BulletScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float moveSpeed = 7f;
+
+    Rigidbody2D rb;
+
+    MovimientoPlayer2 target;
+    Vector2 moveDirection;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindObjectOfType<MovimientoPlayer2>();
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.name.Equals("MovimientoPlayer2"))
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
